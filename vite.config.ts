a99b5a44fe4bdf04de1288c68e856ca734acdc77
Yusuke Nakamura <yaneuranoanmaku@yahoo.co.jp>
2024-05-04 00:52:24 +0900

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
  base: "/remix-tutorial/",
  plugins: [remix({
     basename: "/remix-tutorial/",
+    buildEnd(args) {
+      if (!args.viteConfig.isProduction) return;
+      const buildPath = args.viteConfig.build.outDir;
+      copyFileSync(
+        join(buildPath, "index.html"),
+        join(buildPath, "404.html"),
+      );
+    }
   }), tsconfigPaths()]
 });

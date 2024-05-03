6e064e3a0d902fd4f30276704576572388ff707f
Yusuke Nakamura <yaneuranoanmaku@yahoo.co.jp>
2024-05-04 00:36:12 +0900

    add github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ignoredRouteFiles: ["**/*.css"],
-    }),
-    tsconfigPaths(),
-  ],
+ base: "/deploy-github-pages/",
+ plugins: [remix({
+    ssr: false,
+    basename: "/deploy-github-pages/",
+    buildEnd(args) {
+      if (!args.viteConfig.isProduction) return;
+      const buildPath = args.viteConfig.build.outDir;
+      copyFileSync(
+        join(buildPath, "index.html"),
+        join(buildPath, "404.html"),
+      );
+    }
+  }), tsconfigPaths()]
 });
